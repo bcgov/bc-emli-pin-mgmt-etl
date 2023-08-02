c766c7ebc287819099af2321811a3b39f2447749
Anish Nayanapalli <anayanapalli@gmail.com>
2023-08-01 22:38:57 -0400

    Python black formatted.

--- sftp/config.py
+++ sftp/config.py
@@ -2,6 +2,6 @@ host = "test.rebex.net"
 port = 22
 username = "demo"
 password = "password"
-days_back = 20*365
+days_back = 20 * 365
 remote_path = "/pub/example/"
 local_path = "/Users/anayanapalli/test/"
--- sftp/sftp.py
+++ sftp/sftp.py
@@ -3,8 +3,9 @@ from datetime import datetime, timedelta
 import traceback
 import config as cn
 
+
 def set_sftp_conn(host, port, username, password):
-    """ set sftp connection to get the files, using config.py """
+    """set sftp connection to get the files, using config.py"""
     # connect to sftp
     transport = paramiko.Transport((host, port))
     print("connecting to SFTP...")
@@ -13,8 +14,9 @@ def set_sftp_conn(host, port, username, password):
     print("connection established.")
     return sftp
 
+
 def get_files_to_download_from_sftp(sftp, date_limit, remote_path):
-    """ get the list of files to download from the SFTP server"""
+    """get the list of files to download from the SFTP server"""
     # get files in directory
     all_files = sftp.listdir(remote_path)
     files_to_download = []
@@ -22,21 +24,25 @@ def get_files_to_download_from_sftp(sftp, date_limit, remote_path):
     try:
         for f in all_files:
             last_modified = sftp.stat(remote_path + f).st_mtime
-            last_modified_date = datetime.fromtimestamp(last_modified).date()   
+            last_modified_date = datetime.fromtimestamp(last_modified).date()
             if last_modified_date > date_limit:  # check limit
-                if sftp.stat(remote_path + f).st_size > 500:  # check if file is empty (in this case larger than 500KB)
+                if (
+                    sftp.stat(remote_path + f).st_size > 500
+                ):  # check if file is empty (in this case larger than 500KB)
                     files_to_download.append(f)
         return files_to_download
     except:
         trace_error = traceback.format_exc()
-        print('SOMETHING IS WRONG DIDNT GET THE FILES\n' + trace_error)
+        print("SOMETHING IS WRONG DIDNT GET THE FILES\n" + trace_error)
+
 
 def download_files_from_sftp(sftp, files_list, remote_path, local_path):
     for file in files_list:
-        sftp.get(remote_path+file, local_path+file)
+        sftp.get(remote_path + file, local_path + file)
         print(f"DOWNLOADED----------------{remote_path+file}")
 
-def main():    
+
+def main():
     # set date limit for files
     days_back = cn.days_back
     date_limit = datetime.date(datetime.now()) - timedelta(days=days_back)
@@ -44,13 +50,16 @@ def main():
     # set from & to paths for files to download
     remote_path = cn.remote_path
     local_path = cn.local_path
-    
+
     # establish sftp connection
-    sftp_conn = set_sftp_conn(cn.host, cn.port, cn.username, cn.password)    
-    files_to_download = get_files_to_download_from_sftp(sftp_conn, date_limit, remote_path)
-    
+    sftp_conn = set_sftp_conn(cn.host, cn.port, cn.username, cn.password)
+    files_to_download = get_files_to_download_from_sftp(
+        sftp_conn, date_limit, remote_path
+    )
+
     # download the files
     download_files_from_sftp(sftp_conn, files_to_download, remote_path, local_path)
+
 
 if __name__ == "__main__":
     main()
