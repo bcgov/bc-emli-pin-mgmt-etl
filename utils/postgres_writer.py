4c7e0e89493594becfa80d7bdcb12cdd515d4fbf
Anish Nayanapalli <anayanapalli@gmail.com>
2023-09-07 02:54:39 -0400

    Added a progress bar.

--- utils/postgres_writer.py
+++ utils/postgres_writer.py
@@ -1,9 +1,10 @@
-import argparse
 import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
 import logging
 import datetime
+from tqdm import tqdm
+import sys
 
 
 def setup_logging():
@@ -35,12 +36,22 @@ def write_dataframe_to_postgres(
 
         # Read the DataFrame in chunks
         chunk_size = batch_size
+        total_rows = pd.read_csv(dataframe_file).shape[
+            0
+        ]  # Total number of rows in the DataFrame
+        progress_bar = tqdm(
+            total=total_rows, unit=" rows", unit_scale=True, dynamic_ncols=True
+        )
+
         for chunk in pd.read_csv(
             dataframe_file, chunksize=chunk_size
         ):  # Adjust for different file formats
             # Write the current chunk to the PostgreSQL database
             chunk.to_sql(table_name, engine, if_exists="append", index=False)
+            progress_bar.update(len(chunk))
+            sys.stdout.flush()  # Flush the output to update the progress bar in real-time
 
+        progress_bar.close()
         logging.info(
             f"Data successfully written to the '{table_name}' table in the '{database_name}' database in batches of {batch_size} records each."
         )
@@ -49,49 +60,11 @@ def write_dataframe_to_postgres(
         logging.error(f"An error occurred: {str(e)}")
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description="Write data from a DataFrame to a PostgreSQL database table."
-    )
-    parser.add_argument("dataframe_file", help="Path to the DataFrame file")
-    parser.add_argument("table_name", help="Name of the PostgreSQL table")
-    parser.add_argument("database_name", help="Name of the PostgreSQL database")
-    parser.add_argument(
-        "--batch_size",
-        type=int,
-        default=1000,
-        help="Batch size for writing data (default: 1000)",
-    )
-    parser.add_argument(
-        "--host", default="localhost", help="PostgreSQL host (default: localhost)"
-    )
-    parser.add_argument(
-        "--port", type=int, default=5432, help="PostgreSQL port (default: 5432)"
-    )
-    parser.add_argument(
-        "--user",
-        default="your_username",
-        help="PostgreSQL username (default: your_username)",
-    )
-    parser.add_argument(
-        "--password",
-        default="your_password",
-        help="PostgreSQL password (default: your_password)",
-    )
-
-    args = parser.parse_args()
-    setup_logging()
-    write_dataframe_to_postgres(
-        args.dataframe_file,
-        args.table_name,
-        args.database_name,
-        args.batch_size,
-        args.host,
-        args.port,
-        args.user,
-        args.password,
-    )
-
-
+# Example usage:
 if __name__ == "__main__":
-    main()
+    setup_logging()  # Initialize logging
+    dataframe_file = "your_dataframe.csv"  # Replace with your actual file path
+    table_name = "your_table_name"  # Replace with your desired table name
+    database_name = "your_database_name"  # Replace with your database name
+    batch_size = 1000  # Set your desired batch size
+    write_dataframe_to_postgres(dataframe_file, table_name, database_name, batch_size)
