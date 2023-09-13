3748b435d0002378fbd5345212101e358f99c16f
Anish Nayanapalli <anayanapalli@gmail.com>
2023-09-12 21:47:59 -0400

    Added all the changes.

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,16 @@ RUN chmod g+w ./data
 RUN pip install -r requirements.txt
 USER 1001
 CMD python etl.py \
-        --host=${sftp_host} \
-        --port=${sftp_port} \
-        --username=${sftp_username} \
-        --password=${sftp_password} \
-        --remote_path=${remote_path} \
-        --local_path=${local_path} \
-        --processed_data_path=${processed_data_path}
+        --sftp_host=${sftp_host} \
+        --sftp_port=${sftp_port} \
+        --sftp_username=${sftp_username} \
+        --sftp_password=${sftp_password} \
+        --sftp_remote_path=${sftp_remote_path} \
+        --sftp_local_path=${sftp_local_path} \
+        --processed_data_path=${processed_data_path} \
+        --db_name=${db_name} \
+        --db_write_batch_size=${db_write_batch_size} \
+        --db_host=${db_host} \
+        --db_port=${db_port} \
+        --db_username=${db_username} \
+        --db_password=${db_password}
--- etl.py
+++ etl.py
@@ -1,5 +1,4 @@
-from utils import sftp_downloader
-from utils import sftp_parse
+from utils import sftp_downloader, postgres_writer, sftp_parser
 import argparse
 
 parser = argparse.ArgumentParser(
@@ -9,48 +8,82 @@ parser = argparse.ArgumentParser(
 )
 
 # Host argument
-parser.add_argument("--host", type=str, help="Host address.")
+parser.add_argument("--sftp_host", type=str, help="Host address.")
 
 # Port argument
-parser.add_argument("--port", type=int, help="Port number.")
+parser.add_argument("--sftp_port", type=int, help="Port number.")
 
 # Username argument
-parser.add_argument("--username", type=str, help="Username of the SFTP login.")
+parser.add_argument("--sftp_username", type=str, help="Username of the SFTP login.")
 
 # Password argument
-parser.add_argument("--password", type=str, help="Password of the SFTP login.")
+parser.add_argument("--sftp_password", type=str, help="Password of the SFTP login.")
 
 # Remote path argument
 parser.add_argument(
-    "--remote_path",
+    "--sftp_remote_path",
     type=str,
     help="Remote path of the SFTP folder to copy the files from.",
 )
 
 # Local path argument
 parser.add_argument(
-    "--local_path", type=str, help="Local folder path to download the files to."
+    "--sftp_local_path", type=str, help="Local folder path to download the files to."
 )
 
-# Remote Output Path argument
+# Output folder for the processed csv files
 parser.add_argument(
     "--processed_data_path",
     type=str,
-    help="Remote folder path to download the processed output data files to.",
+    help="Local output folder for the processed csv files.",
 )
 
+
+# db_host argument
+parser.add_argument("--db_host", type=str, help="Host name of the PostgresDB.")
+
+# db_port argument
+parser.add_argument(
+    "--db_port", type=int, default=5432, help="Port number of the Postgres DB."
+)
+
+# db_username argument
+parser.add_argument("--db_username", type=str, help="Username of the SFTP login.")
+
+# db_password argument
+parser.add_argument("--db_password", type=str, help="Password of the SFTP login.")
+
+# db_name argument
+parser.add_argument("--db_name", type=str, help="Name of the DB in the Postgres DB.")
+
+# db_password argument
+parser.add_argument(
+    "--db_write_batch_size", type=int, default=1000, help="Password of the SFTP login."
+)
 
 args = parser.parse_args()
 
 # Step 1: Download the SFTP files to the PVC
 sftp_downloader.run(
-    args.host,
-    args.port,
-    args.username,
-    args.password,
-    args.remote_path,
-    args.local_path,
+    args.sftp_host,
+    args.sftp_port,
+    args.sftp_username,
+    args.sftp_password,
+    args.sftp_remote_path,
+    args.sftp_local_path,
 )
 
 # Step 2: Process the downloaded SFTP files and write to output folder
-sftp_parse.parse_sftp_files(args.local_path, args.processed_data_path)
+sftp_parser.parse_sftp_files(args.local_path, args.processed_data_path)
+
+
+# Step 3: Write the above processed data to the PostgreSQL database
+postgres_writer.run(
+    input_directory=args.processed_data_path,
+    database_name=args.db_name,
+    batch_size=args.db_write_batch_size,
+    host=args.db_host,
+    port=args.db_port,
+    user=args.db_username,
+    password=args.db_password,
+)
