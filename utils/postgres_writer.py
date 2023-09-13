7cd6e11d7399aed1f9e1797ac41a7acde9f53668
Anish Nayanapalli <anayanapalli@gmail.com>
2023-09-12 21:11:58 -0400

    Added more code.

--- utils/postgres_writer.py
+++ utils/postgres_writer.py
@@ -1,16 +1,16 @@
+import os
 import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
 import logging
 import datetime
-from tqdm import tqdm
 import sys
 
 
-def setup_logging():
+def setup_logging(log_file_prefix="postgres_writer"):
     # Create a timestamped log file
     log_file = (
-        f"postgres_writer_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+        f"{log_file_prefix}_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
     )
     logging.basicConfig(
         filename=log_file,
@@ -18,10 +18,35 @@ def setup_logging():
         format="%(asctime)s - %(levelname)s - %(message)s",
     )
 
+    # Define a console handler to display log messages in the console
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)  # Set the desired console log level
+    console_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    console_handler.setFormatter(console_formatter)
 
-def write_dataframe_to_postgres(
-    dataframe_file,
-    table_name,
+    # Attach the console handler to the root logger
+    logging.getLogger().addHandler(console_handler)
+
+
+def write_dataframe_to_postgres(dataframe, table_name, engine, batch_size=1000):
+    try:
+        # Split the dataframe into batches
+        batch_list = [
+            dataframe[i : i + batch_size] for i in range(0, len(dataframe), batch_size)
+        ]
+
+        for i, batch in enumerate(batch_list):
+            batch.to_sql(table_name, engine, if_exists="append", index=False)
+            logging.info(
+                f"Batch {i + 1} of {len(batch_list)} written to the '{table_name}' table."
+            )
+
+    except Exception as e:
+        logging.error(f"An error occurred: {str(e)}")
+
+
+def run(
+    input_directory,
     database_name,
     batch_size=1000,
     host="localhost",
@@ -34,27 +59,22 @@ def write_dataframe_to_postgres(
         conn_str = f"postgresql://{user}:{password}@{host}:{port}/{database_name}"
         engine = create_engine(conn_str)
 
-        # Read the DataFrame in chunks
-        chunk_size = batch_size
-        total_rows = pd.read_csv(dataframe_file).shape[
-            0
-        ]  # Total number of rows in the DataFrame
-        progress_bar = tqdm(
-            total=total_rows, unit=" rows", unit_scale=True, dynamic_ncols=True
-        )
+        # List all files in the input directory
+        file_list = [
+            f
+            for f in os.listdir(input_directory)
+            if os.path.isfile(os.path.join(input_directory, f))
+        ]
 
-        for chunk in pd.read_csv(
-            dataframe_file, chunksize=chunk_size
-        ):  # Adjust for different file formats
-            # Write the current chunk to the PostgreSQL database
-            chunk.to_sql(table_name, engine, if_exists="append", index=False)
-            progress_bar.update(len(chunk))
-            sys.stdout.flush()  # Flush the output to update the progress bar in real-time
+        for file_name in file_list:
+            file_path = os.path.join(input_directory, file_name)
+            df = pd.read_csv(file_path)  # Adjust for different file formats
+            table_name = os.path.splitext(file_name)[
+                0
+            ]  # Use file name without extension as table name
+            write_dataframe_to_postgres(df, table_name, engine, batch_size=batch_size)
 
-        progress_bar.close()
-        logging.info(
-            f"Data successfully written to the '{table_name}' table in the '{database_name}' database in batches of {batch_size} records each."
-        )
+        logging.info("All files processed.")
 
     except Exception as e:
         logging.error(f"An error occurred: {str(e)}")
@@ -63,8 +83,7 @@ def write_dataframe_to_postgres(
 # Example usage:
 if __name__ == "__main__":
     setup_logging()  # Initialize logging
-    dataframe_file = "your_dataframe.csv"  # Replace with your actual file path
-    table_name = "your_table_name"  # Replace with your desired table name
+    input_directory = "your_input_directory"  # Replace with your input directory path
     database_name = "your_database_name"  # Replace with your database name
     batch_size = 1000  # Set your desired batch size
-    write_dataframe_to_postgres(dataframe_file, table_name, database_name, batch_size)
+    run(input_directory, database_name, batch_size=batch_size)
