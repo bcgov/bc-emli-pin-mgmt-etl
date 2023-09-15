2b2fd901b759949c65ef13511fa463446f0acec0
Anish Nayanapalli <anayanapalli@gmail.com>
2023-09-15 01:17:06 -0400

    Made changes to the etl job.

--- etl.py
+++ etl.py
@@ -68,6 +68,13 @@ parser.add_argument(
     help="Number of records to be written to the db in one batch.",
 )
 
+# data_rules_url argument
+parser.add_argument(
+    "--data_rules_url",
+    type=str,
+    help="URL to the data_rules.json file in a public GitHub repository.",
+)
+
 args = parser.parse_args()
 
 # Step 1: Download the SFTP files to the PVC
@@ -81,17 +88,19 @@ sftp_downloader.run(
 )
 
 # Step 2: Process the downloaded SFTP files and write to output folder
-ltsa_parser.parse_sftp_files(
-    input_directory=args.sftp_local_path, output_directory=args.processed_data_path
+ltsa_parser.run(
+    input_directory=args.sftp_local_path,
+    output_directory=args.processed_data_path,
+    data_rules_url=args.data_rules_url,
 )
 
 # Step 3: Write the above processed data to the PostgreSQL database
-postgres_writer.run(
-    input_directory=args.processed_data_path,
-    database_name=args.db_name,
-    batch_size=args.db_write_batch_size,
-    host=args.db_host,
-    port=args.db_port,
-    user=args.db_username,
-    password=args.db_password,
-)
+# postgres_writer.run(
+#     input_directory=args.processed_data_path,
+#     database_name=args.db_name,
+#     batch_size=args.db_write_batch_size,
+#     host=args.db_host,
+#     port=args.db_port,
+#     user=args.db_username,
+#     password=args.db_password,
+# )
