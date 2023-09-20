01d9c7625510e05b5b57666ec1657ee077f3fdb3
Anish Nayanapalli <anayanapalli@gmail.com>
2023-09-20 01:50:58 -0400

    Added a timer.

--- utils/postgres_writer.py
+++ utils/postgres_writer.py
@@ -1,6 +1,6 @@
-import os
 import pandas as pd
 from sqlalchemy import create_engine
+import time
 
 
 def write_dataframe_to_postgres(dataframe, table_name, engine, batch_size=1000):
@@ -14,22 +14,27 @@ def write_dataframe_to_postgres(dataframe, table_name, engine, batch_size=1000):
     - batch_size (int, optional): Number of rows to write in each batch. Default is 1000.
 
     Returns:
-    - None
+    - int: The total number of rows inserted into the table.
     """
+    total_rows_inserted = 0  # Initialize the total count of rows inserted
     try:
+        print(f"Updating table '{table_name}'...")  # Print the table being updated
+
         # Split the dataframe into batches
         batch_list = [
             dataframe[i : i + batch_size] for i in range(0, len(dataframe), batch_size)
         ]
 
         for i, batch in enumerate(batch_list):
+            rows_inserted = len(batch)
             batch.to_sql(table_name, engine, if_exists="append", index=False)
-            print(
-                f"Batch {i + 1} of {len(batch_list)} written to the '{table_name}' table."
-            )
+            total_rows_inserted += rows_inserted  # Update the total count
+
+        return total_rows_inserted  # Return the total count of rows inserted
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
+        return 0
 
 
 def run(
@@ -68,15 +73,41 @@ def run(
             if os.path.isfile(os.path.join(input_directory, f))
         ]
 
+        table_statistics = []  # List to store table-wise statistics
+
+        start_time = time.time()  # Start measuring time
+
         for file_name in file_list:
             file_path = os.path.join(input_directory, file_name)
             # Adjust for different file formats (e.g., pd.read_csv for CSV files)
             df = pd.read_csv(file_path)
             # Use file name without extension as table name
             table_name = os.path.splitext(file_name)[0]
-            write_dataframe_to_postgres(df, table_name, engine, batch_size=batch_size)
+            rows_inserted = write_dataframe_to_postgres(
+                df, table_name, engine, batch_size=batch_size
+            )
+            elapsed_time = time.time() - start_time
+            table_statistics.append(
+                {
+                    "Table Name": table_name,
+                    "Rows Inserted": rows_inserted,
+                    "Elapsed Time (s)": elapsed_time,
+                }
+            )
 
-        print("All files processed.")
+        end_time = time.time()  # Stop measuring time
+
+        total_rows_inserted = sum(stat["Rows Inserted"] for stat in table_statistics)
+
+        print("Table-wise Statistics:")
+        for stat in table_statistics:
+            print(
+                f"Table: {stat['Table Name']}, Rows Inserted: {stat['Rows Inserted']}, Elapsed Time: {stat['Elapsed Time (s)']:.2f} seconds"
+            )
+
+        print(
+            f"All files processed. Total rows inserted: {total_rows_inserted}, Total Time elapsed: {end_time - start_time:.2f} seconds"
+        )
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
