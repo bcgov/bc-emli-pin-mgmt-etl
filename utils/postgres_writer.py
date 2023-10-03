fa25bb2da7def41972457702d23733347a5f6ef3
Anish Nayanapalli <anayanapalli@gmail.com>
2023-10-03 11:50:45 -0400

    Made a few minor changes to postgres_writer.

--- utils/postgres_writer.py
+++ utils/postgres_writer.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 from sqlalchemy import text, create_engine, func, select
 import time
-import psycopg2, os
+import psycopg2
+import os
 
 
 def insert_postgres_table_if_rows_not_exist(
@@ -11,17 +12,16 @@ def insert_postgres_table_if_rows_not_exist(
     """
     Inserts non-duplicate rows into PostreSQL table in batches.
 
-    Parameters:
-    - dataframe (pd.DataFrame): The DataFrame to be written.
-    - table_name (str): The name of the PostgreSQL table.
-    - engine (sqlalchemy.engine.base.Engine): SQLAlchemy engine for database connection.
-    - unique_key_columns (list): Columns that will prevent data insert on conflict.
+    Args:
+        dataframe (pd.DataFrame): The DataFrame to be written.
+        table_name (str): The name of the PostgreSQL table.
+        engine (sqlalchemy.engine.base.Engine): SQLAlchemy engine for database connection.
+        unique_key_columns (list): Columns that will prevent data insert on conflict.
 
     Returns:
-    - None (or Error)
+        None (or Error)
     """
     try:
-        # Create a list of column names as a comma-separated string
         column_names = ", ".join(dataframe.columns)
 
         # Convert the DataFrame to a list of tuples for insertion
@@ -38,11 +38,11 @@ def insert_postgres_table_if_rows_not_exist(
         )
 
         # Create a SQL INSERT statement with ON CONFLICT DO NOTHING clause
-        insert_sql = f"INSERT INTO {table_name} ({column_names}) VALUES {data_to_insert} ON CONFLICT ({', '.join(unique_key_columns)}) DO NOTHING;"
+        insert_sql = f"INSERT INTO {table_name} ({column_names}) VALUES %s ON CONFLICT ({', '.join(unique_key_columns)}) DO NOTHING;"
 
         # Execute the SQL statement with parameter binding
         with engine.begin() as conn:
-            conn.execute(text(insert_sql))
+            conn.execute(text(insert_sql), data_to_insert)
 
     except Exception as e:
         return f"Error: {str(e)}"
@@ -52,17 +52,16 @@ def get_row_count(table_name, engine):
     """
     Counts number of rows in database table.
 
-    Parameters:
-    - table_name (str): The name of the PostgreSQL table.
-    - engine (sqlalchemy.engine.base.Engine): SQLAlchemy engine for database connection.
+    Args:
+        table_name (str): The name of the PostgreSQL table.
+        engine (sqlalchemy.engine.base.Engine): SQLAlchemy engine for database connection.
 
     Returns:
-    - int:  The total number of rows in the table.
+        int:  The total number of rows in the table.
     """
     query = select([func.count()]).select_from(text(table_name))
     conn = engine.connect()
     totalCount = conn.execute(query).fetchone()[0]
-    print(query)  # Remove after debugging
     return totalCount
 
 
@@ -70,16 +69,16 @@ def write_dataframe_to_postgres(dataframe, table_name, engine, batch_size=1000):
     """
     Write a DataFrame to a PostgreSQL table in batches.
 
-    Parameters:
-    - dataframe (pd.DataFrame): The DataFrame to be written.
-    - table_name (str): The name of the PostgreSQL table.
-    - engine (sqlalchemy.engine.base.Engine): SQLAlchemy engine for database connection.
-    - batch_size (int, optional): Number of rows to write in each batch. Default is 1000.
+    Args:
+        dataframe (pd.DataFrame): The DataFrame to be written.
+        table_name (str): The name of the PostgreSQL table.
+        engine (sqlalchemy.engine.base.Engine): SQLAlchemy engine for database connection.
+        batch_size (int, optional): Number of rows to write in each batch. Default is 1000.
 
     Returns:
-    - int: The total number of rows inserted into the table.
+        int: The total number of rows inserted into the table.
     """
-    total_rows_inserted = 0  # Initialize the total count of rows inserted
+    total_rows_inserted = 0
     try:
         print(f"Updating table '{table_name}'...")  # Print the table being updated
 
@@ -91,7 +90,6 @@ def write_dataframe_to_postgres(dataframe, table_name, engine, batch_size=1000):
             dataframe[i : i + batch_size] for i in range(0, len(dataframe), batch_size)
         ]
 
-        # Define the columns that make up the unique key --all columns
         unique_key_columns = dataframe.columns.tolist()
 
         for batch in batches:
@@ -106,7 +104,7 @@ def write_dataframe_to_postgres(dataframe, table_name, engine, batch_size=1000):
         rows_after_insert = get_row_count(table_name, engine)
         total_rows_inserted = rows_after_insert - rows_before_insert
 
-        return total_rows_inserted  # Return the total count of rows inserted
+        return total_rows_inserted
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
@@ -125,40 +123,43 @@ def run(
     """
     Process files in a directory and write them to a PostgreSQL database.
 
-    Parameters:
-    - input_directory (str): The path to the directory containing input files.
-    - database_name (str): The name of the PostgreSQL database.
-    - batch_size (int, optional): Number of rows to write in each batch. Default is 1000.
-    - host (str, optional): The database host. Default is "localhost".
-    - port (int, optional): The database port. Default is 5432.
-    - user (str, optional): The database username. Default is "your_username".
-    - password (str, optional): The database password. Default is "your_password".
+    Args:
+        input_directory (str): The path to the directory containing input files.
+        database_name (str): The name of the PostgreSQL database.
+        batch_size (int, optional): Number of rows to write in each batch. Default is 1000.
+        host (str, optional): The database host. Default is "localhost".
+        port (int, optional): The database port. Default is 5432.
+        user (str, optional): The database username. Default is "your_username".
+        password (str, optional): The database password. Default is "your_password".
 
     Returns:
-    - None
+        None
     """
     try:
-        # Create a connection to the PostgreSQL database
         conn_str = f"postgresql://{user}:{password}@{host}:{port}/{database_name}"
         engine = create_engine(conn_str)
 
-        # List all files in the input directory
         file_list = [
             f
             for f in os.listdir(input_directory)
             if (os.path.isfile(os.path.join(input_directory, f)) and f.endswith("csv"))
         ]
 
-        table_statistics = []  # List to store table-wise statistics
+        # List to store table-wise statistics
+        table_statistics = []
 
-        start_time = time.time()  # Start measuring time
+        # Start measuring time
+        start_time = time.time()
 
         for file_name in file_list:
             file_path = os.path.join(input_directory, file_name)
+
             # Adjust for different file formats (e.g., pd.read_csv for CSV files)
             df = pd.read_csv(file_path, encoding="unicode_escape", low_memory=False)
+
             # Use file name without extension as table name
             table_name = os.path.splitext(file_name)[0]
+
             rows_inserted = write_dataframe_to_postgres(
                 df, table_name, engine, batch_size=batch_size
             )
@@ -171,7 +172,8 @@ def run(
                 }
             )
 
-        end_time = time.time()  # Stop measuring time
+        # Stop measuring time
+        end_time = time.time()
 
         total_rows_inserted = sum(stat["Rows Inserted"] for stat in table_statistics)
 
@@ -189,18 +191,15 @@ def run(
         print(f"An error occurred: {str(e)}")
 
 
-# Entry point of the script
 if __name__ == "__main__":
-    # Replace these placeholders with your actual values
-    input_directory = "your_input_directory"  # Replace with your input directory path
-    database_name = "your_database_name"  # Replace with your database name
-    batch_size = 1000  # Set your desired batch size
-    host = "localhost"  # Replace with your host
-    port = 5432  # Replace with your port
-    user = "your_username"  # Replace with your username
-    password = "your_password"  # Replace with your password
+    input_directory = "your_input_directory"
+    database_name = "your_database_name"
+    batch_size = 1000
+    host = "localhost"
+    port = 5432
+    user = "your_username"
+    password = "your_password"
 
-    # Call the run function with the specified parameters
     run(
         input_directory,
         database_name,
