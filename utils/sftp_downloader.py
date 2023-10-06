6fefa7b69bcfe82fcd9cdaac1974754c2cb304bb
emilymendelson <emendelson@deloitte.ca>
2023-10-06 11:14:24 -0400

    first attempt at finding latest LTSA folder from SFTP server

--- utils/sftp_downloader.py
+++ utils/sftp_downloader.py
@@ -26,39 +26,59 @@ def set_sftp_conn(host, port, username, password):
         raise e
 
 
-def get_files_to_download_from_sftp(sftp, remote_path):
+def get_files_to_download_from_sftp(sftp):
     """
     Get the list of files to download from the SFTP server.
 
     Args:
         sftp (paramiko.SFTPClient): An SFTP client object.
-        remote_path (str): The remote directory path on the SFTP server.
 
     Returns:
         list of str: A list of filenames to download.
     """
     try:
-        files_to_download = sftp.listdir(remote_path)
-        if not files_to_download:
+        latest = 0
+        latestfolder = None
+
+        # For each file/folder in in '/export/' directory
+        # Update this to not be hardcoded after testing
+        for fileattr in sftp.listdir_attr("/export/"):
+            # Compare folder timestamps for each file in directory to find latest folder
+            if fileattr.st_mtime > latest:
+                latest = fileattr.st_mtime
+                latestfolder = fileattr.filename
+
+        if latestfolder is not None:
+            print(f"latestfolder: {latestfolder}")
+
+            files_to_download = sftp.listdir(f"/export/{latestfolder}/{latestfolder}/")
+            file_paths_dict = {}
+            print(f"files_to_download: {files_to_download}")
+
+            for file in files_to_download:
+                file_paths_dict[file] = f"/export/{latestfolder}/{latestfolder}/{file}"
+            print(f"file_paths_dict: {file_paths_dict}")
+
+        else:
             print("No new files uploaded...")
-        return files_to_download
+
+        return file_paths_dict
     except Exception as e:
         raise e
 
 
-def download_files_from_sftp(sftp, files_list, remote_path, local_path):
+def download_files_from_sftp(sftp, file_paths_dict, local_path):
     """
     Download files from SFTP server to the local directory.
 
     Args:
         sftp (paramiko.SFTPClient): An SFTP client object.
-        files_list (list of str): A list of filenames to download.
-        remote_path (str): The remote directory path on the SFTP server.
+        file_paths_dict (dict): Key is file name to download, value is full file path to download.
         local_path (str): The local directory path where files will be downloaded.
     """
     try:
-        for file in files_list:
-            remote_file_path = remote_path + file
+        for file, file_path in file_paths_dict.items():
+            remote_file_path = file_path
             local_file_path = local_path + file
             sftp.get(remote_file_path, local_file_path)
             print(f"Downloaded: {remote_file_path} -> {local_file_path}")
@@ -83,10 +103,10 @@ def run(host, port, username, password, remote_path, local_path):
         # Establish SFTP connection
         sftp_conn = set_sftp_conn(host, port, username, password)
 
-        files_to_download = get_files_to_download_from_sftp(sftp_conn, remote_path)
+        file_paths_dict = get_files_to_download_from_sftp(sftp_conn)
 
         # Download the files
-        download_files_from_sftp(sftp_conn, files_to_download, remote_path, local_path)
+        download_files_from_sftp(sftp_conn, file_paths_dict, local_path)
 
     except Exception as e:
         print(f"Error downloding LTSA files: {str(e)}")
